9bacb0aac2c4559d373943fd979cf3ec5275d2f5
Erekle Roinishvili <erekleroinishvili@gmail.com>
2025-01-22 00:31:58 +0000

    Questions updated, `README.md` updated to match

--- README.md
+++ README.md
@@ -16,15 +16,57 @@ There are five question types supported:
 
 * `ask`, `bid`, `ask-bid`, `ask-bid-same`, and `prompt`
 
+The first four question types (`ask`, `bid`, `ask-bid`, and `ask-bid-same`)
+match specific trading situations and have graphical representation acompanying them.
+
+The last question type (`prompt`) is a free textual question without graphical representation.
+
+### Question Generation
+
+Questions are generated from templates by filling in placeholder variables with randomly
+generated values. In graphical questions, only predifined variables can be used:
+
+`{ask-volume}`, `{ask-price}`, `{bid-volume}`, and `{bid-price}`
+
+They will be filled with randomly generated values matching the graphical presentation of
+the question.
+
+`{ask-volume}` and `{bid-volume}` will always be set to a whole number.
+`{ask-price}` and `{bid-price}` may be set to a fraction, such as `10.75`
+
+In `prompt` or free text questions, any variable name can be used. If a variable name
+starts with `$`, such as `{$ask-price}`, it may be set to a fraction. Otherwise
+it will be set to a whole number. [See Question `prompt`](#question-prompt)
+
 #### Question `ask`
+
+Offer to sell.
 
 #### Question `bid`
 
+Offer to buy.
+
 #### Question `ask-bid`
+
+Offer to buy and sell.
 
 #### Question `ask-bid-same`
 
+Offer to buy and sell, both offered at the same volume.
+
 #### Question `prompt`
+
+This type of question does not have graphics and allows generating text questions.
+The `prompt` key can be a string or an array of strings. It can use any variable names,
+consisting of a variable name in a curly brackets.
+
+E.g.: `{n}`, `{volume}`, `{bid-volume}`, `{ask-price}`, `{$bid-price}`
+
+If a variable name starts with a `$` (as in `${bid-price}`), it will be treated as a currency
+and may be set to a fractional value, such as `12.75`
+
+The same variables should be used in the `reply`, where they will be set to the same
+values as they were set in the `prompt`
 
 ### Config File
 
@@ -35,7 +77,7 @@ Questions are configured in the file `src/app/questions.config.ts` ([view](src/a
 ```TypeScript
 export const PROMPTS: WeightedPrompt[] = [
     {
-        weight: 1, // Questions with higher weight appear more often
+        weight: 10, // Questions with higher weight appear more often
         prompt: { // Question specification
             // Question type. 'ask', 'bid', 'ask-bid, 'ask-bid-same', 'prompt'
             type: 'ask', // Offer to sell
@@ -43,12 +85,13 @@ export const PROMPTS: WeightedPrompt[] = [
             cancel: false, // Optional; Trader is canceling offer
             reply: [ // One or more intended phrasing for trader to articulate offer
                 // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '{ask-volume} offered at ${ask-price}',
                 '{ask-volume} at ${ask-price}',
             ],
         },
     },
     {
-        weight: 1, // Questions with higher weight appear more often
+        weight: 10, // Questions with higher weight appear more often
         prompt: { // Question specification
             // Question type. 'ask', 'bid', 'ask-bid, 'ask-bid-same', 'prompt'
             type: 'bid', // Offer to buy
@@ -57,11 +100,12 @@ export const PROMPTS: WeightedPrompt[] = [
             reply: [ // One or more intended phrasing for trader to articulate offer
                 // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
                 '${bid-price} bid for {bid-volume}',
+                '${bid-price} for {bid-volume}',
             ],
         },
     },
     {
-        weight: 1, // Questions with higher weight appear more often
+        weight: 5, // Questions with higher weight appear more often
         prompt: { // Question specification
             // Question type. 'ask', 'bid', 'ask-bid, 'ask-bid-same', 'prompt'
             type: 'ask-bid', // Two offers: to buy & sell. Different volumes.
@@ -69,12 +113,14 @@ export const PROMPTS: WeightedPrompt[] = [
             cancel: false, // Optional; Trader is canceling offer
             reply: [ // One or more intended phrasing for trader to articulate offer
                 // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '${bid-price} bid for {bid-volume}, {ask-volume} offered at ${ask-price}',
                 '${bid-price} bid for {bid-volume}, {ask-volume} at ${ask-price}',
+                '${bid-price} for {bid-volume}, {ask-volume} at ${ask-price}',
             ],
         },
     },
     {
-        weight: 1, // Questions with higher weight appear more often
+        weight: 5, // Questions with higher weight appear more often
         prompt: { // Question specification
             // Question type. 'ask', 'bid', 'ask-bid, 'ask-bid-same', 'prompt'
             type: 'ask-bid-same', // Two offers: to buy & sell. Same volumes.
@@ -82,7 +128,7 @@ export const PROMPTS: WeightedPrompt[] = [
             cancel: false, // Optional; Trader is canceling offer
             reply: [
                 // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
-                '⛔ Apples!',
+                '${bid-price} at ${ask-price}, {ask-volume} up',
             ],
         },
     },
--- src/app/questions.config.ts
+++ src/app/questions.config.ts
@@ -2,17 +2,116 @@ import { WeightedPrompt } from "./models/weighted-prompt.model"
 
 export const PROMPTS: WeightedPrompt[] = [
     {
-        weight: 1,
+        weight: 10,
         prompt: {
             type: 'ask',
             reply: [
                 // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '{ask-volume} offered at ${ask-price}',
                 '{ask-volume} at ${ask-price}',
             ],
         },
     },
     {
+        weight: 10,
+        prompt: {
+            type: 'bid',
+            reply: [
+                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '${bid-price} bid for {bid-volume}',
+                '${bid-price} for {bid-volume}',
+            ],
+        },
+    },
+    {
+        weight: 5,
+        prompt: {
+            type: 'ask-bid',
+            reply: [
+                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '${bid-price} bid for {bid-volume}, {ask-volume} offered at ${ask-price}',
+                '${bid-price} bid for {bid-volume}, {ask-volume} at ${ask-price}',
+                '${bid-price} for {bid-volume}, {ask-volume} at ${ask-price}',
+            ],
+        },
+    },
+    {
+        weight: 5,
+        prompt: {
+            type: 'ask-bid-same',
+            reply: [
+                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                '${bid-price} at ${ask-price}, {ask-volume} up',
+            ],
+        },
+    },
+    {
         weight: 1,
+        prompt: {
+            type: 'ask-bid',
+            cancel: true,
+            reply: [
+                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
+                'I\'m out!',
+                'I am out!',
+            ],
+        },
+    },
+    {
+        weight: 1,
+        prompt: {
+            type: 'prompt',
+            prompt: [
+                'I am buying all the contracts on the offer',
+            ],
+            reply: [
+                // Use: {any-var} for volumes and {$any-var} for prices. E.g.: {ask-volume} {$ask-price} {bid-volume} {$bid-price}
+                'Buy \'em!',
+                'Take \'em',
+            ],
+        }
+    },
+    {
+        weight: 1,
+        prompt: {
+            type: 'prompt',
+            prompt: [
+                'I am buying {bid-volume} contracts',
+            ],
+            reply: [
+                // Use: {any-var} for volumes and {$any-var} for prices. E.g.: {ask-volume} {$ask-price} {bid-volume} {$bid-price}
+                'Buy {bid-volume}!',
+            ],
+        }
+    },
+    {
+        weight: 1,
+        prompt: {
+            type: 'prompt',
+            prompt: [
+                'I am selling all the contracts on the bid',
+            ],
+            reply: [
+                // Use: {any-var} for volumes and {$any-var} for prices. E.g.: {ask-volume} {$ask-price} {bid-volume} {$bid-price}
+                'Sold!',
+            ],
+        }
+    },
+    {
+        weight: 1,
+        prompt: {
+            type: 'prompt',
+            prompt: [
+                'I am selling {ask-volume} contracts',
+            ],
+            reply: [
+                // Use: {any-var} for volumes and {$any-var} for prices. E.g.: {ask-volume} {$ask-price} {bid-volume} {$bid-price}
+                'Sell {ask-volume}!',
+            ],
+        }
+    },
+    {
+        weight: 0,
         prompt: {
             type: 'ask',
             query: true,
@@ -23,7 +122,7 @@ export const PROMPTS: WeightedPrompt[] = [
         },
     },
     {
-        weight: 1,
+        weight: 0,
         prompt: {
             type: 'ask',
             cancel: true,
@@ -34,17 +133,7 @@ export const PROMPTS: WeightedPrompt[] = [
         },
     },
     {
-        weight: 1,
-        prompt: {
-            type: 'bid',
-            reply: [
-                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
-                '${bid-price} bid for {bid-volume}',
-            ],
-        },
-    },
-    {
-        weight: 1,
+        weight: 0,
         prompt: {
             type: 'bid',
             query: true,
@@ -56,7 +145,7 @@ export const PROMPTS: WeightedPrompt[] = [
         },
     },
     {
-        weight: 1,
+        weight: 0,
         prompt: {
             type: 'bid',
             cancel: true,
@@ -67,17 +156,7 @@ export const PROMPTS: WeightedPrompt[] = [
         },
     },
     {
-        weight: 1,
-        prompt: {
-            type: 'ask-bid',
-            reply: [
-                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
-                '${bid-price} bid for {bid-volume}, {ask-volume} at ${ask-price}',
-            ],
-        },
-    },
-    {
-        weight: 1,
+        weight: 0,
         prompt: {
             type: 'ask-bid',
             query: true,
@@ -90,28 +169,7 @@ export const PROMPTS: WeightedPrompt[] = [
         },
     },
     {
-        weight: 1,
-        prompt: {
-            type: 'ask-bid',
-            cancel: true,
-            reply: [
-                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
-                '⛔ Apples!',
-            ],
-        },
-    },
-    {
-        weight: 1,
-        prompt: {
-            type: 'ask-bid-same',
-            reply: [
-                // Use: {ask-volume} {ask-price} {bid-volume} {bid-price}
-                '⛔ Apples!',
-            ],
-        },
-    },
-    {
-        weight: 1,
+        weight: 0,
         prompt: {
             type: 'prompt',
             prompt: 'Can I use {any-var} for volumes and {$any-var} for prices. E.g.: {ask-volume} {$ask-price} {bid-volume} {$bid-price}',
